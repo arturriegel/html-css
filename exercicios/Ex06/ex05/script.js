d3b52f9092226114ab417abef3de9fe6101afb3b
arturriegel <134106239+arturriegel@users.noreply.github.com>
2023-08-08 16:52:59 -0300

    CAP25 E11

---
+++ exercicios/Ex06/ex05/script.js
@@ -0,0 +1,8 @@
+function clicar() {
+    var menu = document.querySelector('menu#hbg')
+    if (menu.style.display == 'block') {
+        menu.style.display = 'none'
+    } else {
+        menu.style.display = 'block'
+    }
+}
